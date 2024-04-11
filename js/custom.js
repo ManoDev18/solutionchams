67060e47c9512fc1ab91680866407dbef4fd59ac
ManoDev18 <166386236+ManoDev18@users.noreply.github.com>
2024-04-11 07:53:20 +0530

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,121 @@
+$(document).ready(function(){
+
+    $('.testimonials_section').slick({
+        slidesToShow: 1,
+        slidesToScroll:1,
+        centerMode: true,
+        centerPadding: '300px',
+        speed: 2000,
+        autoplay: false,
+        arrows:true,
+        dots: true,
+        responsive: [
+            {
+                breakpoint: 992,
+                 settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 1,
+                        speed:2000,
+                        infinite: true,
+                        arrows:false,
+                        dots: false
+                    }
+            },
+          
+            {
+                breakpoint: 767,
+                 settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                        infinite: true,
+                        arrows:false,
+                        dots:false
+                    }
+            },
+            {
+                breakpoint: 480,
+                 settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                        infinite: true,
+                        arrows:false,
+                    }
+            }
+        ]
+    });	
+
+    $('.logo_slider').slick({
+        slidesToShow: 6,
+        slidesToScroll:1,
+        speed: 2000,
+        autoplay: false,
+        arrows:false,
+        dots: false,
+        responsive: [
+            {
+                breakpoint: 992,
+                 settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 1,
+                        speed:2000,
+                        infinite: true,
+                        arrows:false,
+                        dots: false
+                    }
+            },
+          
+            {
+                breakpoint: 767,
+                 settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                        infinite: true,
+                        arrows:false,
+                        dots:false
+                    }
+            },
+            {
+                breakpoint: 480,
+                 settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                        infinite: true,
+                        arrows:false,
+                    }
+            }
+        ]
+    });	
+
+});
+
+/*********accrodian*********/
+$(document).ready(function () {
+	$(".set > a").on("click", function () {
+		if ($(this).hasClass("active")) {
+            $(this).parent(".set").removeClass("active");
+			$(this).removeClass("active");
+			$(this)
+				.siblings(".content")
+				.slideUp(200);
+			$(".set > a i")
+				.removeClass("fa-minus")
+				.addClass("fa-plus");
+		} else {
+            $(this).parent(".set").addClass("active");
+			$(".set > a i")
+				.removeClass("fa-minus")
+				.addClass("fa-plus");
+			$(this)
+				.find("i")
+				.removeClass("fa-plus")
+				.addClass("fa-minus");
+			$(".set > a").removeClass("active");
+			$(this).addClass("active");
+			$(".content").slideUp(200);
+			$(this)
+				.siblings(".content")
+				.slideDown(200);
+		}
+	
+    });
+});
